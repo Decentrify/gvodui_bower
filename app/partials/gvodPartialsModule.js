6c9a1a23499acbcf5f0538442621bfa8621139d7
Alex Ormenisan <ormenisan.adrian@gmail.com>
2016-09-06 17:25:34 +0200

    working graphs

--- app/partials/gvodPartialsModule.js
+++ app/partials/gvodPartialsModule.js
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 
-angular.module('partialsApplication', ['nvd3']);
+angular.module('partialsApplication', ['nvd3', 'ngCsvImport']);
 
 angular.module('partialsApplication').factory('nRestCalls', ['$http', 'nRestServerState', 'nHdfsEndpoint',
     function ($http, nRestServerState, nHdfsEndpoint) {
@@ -479,125 +479,116 @@ angular.module('partialsApplication').controller('NVoDHopsController', ['nRestCa
     }]);
 
 angular.module('partialsApplication').controller('NGraphsController', function($scope){
-
-        /* Chart options */
-        $scope.options = {
-            chart: {
-                type: 'lineChart',
-                height: 450,
-                margin : {
-                    top: 20,
-                    right: 20,
-                    bottom: 40,
-                    left: 55
-                },
-                x: function(d){ return d.x; },
-                y: function(d){ return d.y; },
-                useInteractiveGuideline: true,
-                dispatch: {
-                    stateChange: function(e){ console.log("stateChange"); },
-                    changeState: function(e){ console.log("changeState"); },
-                    tooltipShow: function(e){ console.log("tooltipShow"); },
-                    tooltipHide: function(e){ console.log("tooltipHide"); }
-                },
-                xAxis: {
-                    axisLabel: 'Time (ms)'
-                },
-                yAxis: {
-                    axisLabel: 'Voltage (v)',
-                    tickFormat: function(d){
-                        return d3.format('.02f')(d);
-                    },
-                    axisLabelDistance: -10
-                },
-                callback: function(chart){
-                    console.log("!!! lineChart callback !!!");
-                }
+    $scope.loadContent = {
+        content: null,
+        header: false,
+        headerVisible: false,
+        separator: ',',
+        separatorVisible: false,
+        result: null,
+        encoding: 'ISO-8859-1',
+        encodingVisible: false,
+        callback : function() {
+        }
+    }; 
+    $scope.transferContent = {
+        content: null,
+        header: false,
+        headerVisible: false,
+        separator: ',',
+        separatorVisible: false,
+        result: null,
+        encoding: 'ISO-8859-1',
+        encodingVisible: false,
+        callback : function() {
+        }
+    }; 
+    $scope.canPlot = false;
+    $scope.plot = function() {
+        var queueLoad = [];
+        var bufferSize = [];
+        var transferSize = [];
+        var downSpeed =[];
+        var upSpeed =[];
+        //$scope.loadContent.result.length
+        //parseInt($scope.loadContent.result[i][0])
+        for(var i = 0; i < 100; i++) {
+            queueLoad.push({x: i, y: parseInt($scope.loadContent.result[i][0])});
+            bufferSize.push({x: i, y: parseInt($scope.loadContent.result[i][1])});
+            transferSize.push({x: i, y: parseInt($scope.loadContent.result[i][2])});
+            downSpeed.push({x: i, y: parseInt($scope.transferContent.result[i][1])/1024});
+            upSpeed.push({x: i, y: parseInt($scope.transferContent.result[i][2])/1024});
+        }
+        $scope.qdData = [{key: "ql", values:queueLoad}]; 
+        $scope.bufData = [{key: "buffer", values:bufferSize}, {key: "transfer", values:transferSize}]; 
+        $scope.speedData = [{key: "down", values:downSpeed}, {key: "up", values:upSpeed}]; 
+        $scope.canPlot=true; 
+    };  
+        
+    /* Chart options */
+    $scope.options = function() {
+        return {
+        chart: {
+            type: 'lineChart',
+            height: 450,
+            margin : {
+                top: 20,
+                right: 20,
+                bottom: 40,
+                left: 200
             },
-            title: {
-                enable: true,
-                text: 'Title for Line Chart'
+            x: function(d){ return d.x; },
+            y: function(d){ return d.y; },
+            useInteractiveGuideline: true,
+            dispatch: {
+                stateChange: function(e){ console.log("stateChange"); },
+                changeState: function(e){ console.log("changeState"); },
+                tooltipShow: function(e){ console.log("tooltipShow"); },
+                tooltipHide: function(e){ console.log("tooltipHide"); }
+            },
+            xAxis: {
+                axisLabel: 'Time (s)'
+            },
+            yAxis: {
+                axisLabel: 'y label',
+                tickFormat: function(d){
+                    return d3.format('d')(d);
+                },
+                axisLabelDistance: -10,
+                showMaxMin: true
+            },
+            callback: function(chart){
+                console.log("!!! chart callback !!!");
             },
             subtitle: {
-                enable: true,
-                text: 'Subtitle for simple line chart. Lorem ipsum dolor sit amet, at eam blandit sadipscing, vim adhuc sanctus disputando ex, cu usu affert alienum urbanitas.',
-                css: {
-                    'text-align': 'center',
-                    'margin': '10px 13px 0px 7px'
-                }
+                "enable": false,
             },
             caption: {
-                enable: true,
-                html: '<b>Figure 1.</b> Lorem ipsum dolor sit amet, at eam blandit sadipscing, <span style="text-decoration: underline;">vim adhuc sanctus disputando ex</span>, cu usu affert alienum urbanitas. <i>Cum in purto erat, mea ne nominavi persecuti reformidans.</i> Docendi blandit abhorreant ea has, minim tantas alterum pro eu. <span style="color: darkred;">Exerci graeci ad vix, elit tacimates ea duo</span>. Id mel eruditi fuisset. Stet vidit patrioque in pro, eum ex veri verterem abhorreant, id unum oportere intellegam nec<sup>[1, <a href="https://github.com/krispo/angular-nvd3" target="_blank">2</a>, 3]</sup>.',
-                css: {
-                    'text-align': 'justify',
-                    'margin': '10px 13px 0px 7px'
-                }
+                "enable": false,
             }
+        },
+        title: {
+            enable: true,
+            text: 'Title'
+        },
         };
+    };
+    $scope.qdOptions = $scope.options();
+    $scope.qdOptions.chart.yAxis.axisLabel = 'queue time(s)';
+    $scope.qdOptions.title.text = 'queue delay';
+    $scope.bufOptions = $scope.options();
+    $scope.bufOptions.chart.yAxis.axisLabel = 'buffer size(blocks)';
+    $scope.bufOptions.title.text = 'buffers';
+    $scope.speedOptions = $scope.options();
+    $scope.speedOptions.chart.yAxis.axisLabel = 'transfer speed (KB)';
+    $scope.speedOptions.title.text = 'speed';
+    // $scope.bufOptions.chart.yDomain = [0,20,40,60];
 
-        /* Chart data */
-        $scope.data = [{values: [{x:0,y:1},{x:1,y:1},{x:2,y:2},{x:3,y:3}], key: 'l1'}, {values: [{x:0,y:3},{x:1,y:3},{x:2,y:4},{x:3,y:5}], key: 'l2'}]
+    // $scope.data = [{values: [{x:0,y:1},{x:1,y:1},{x:2,y:2},{x:3,y:3}], key: 'l1'}, {values: [{x:0,y:3},{x:1,y:3},{x:2,y:4},{x:3,y:5}], key: 'l2'}];
 
-        /*Random Data Generator */
-        function sinAndCos() {
-            var sin = [],sin2 = [],
-                cos = [];
-
-            //Data is represented as an array of {x,y} pairs.
-            for (var i = 0; i < 100; i++) {
-                sin.push({x: i, y: Math.sin(i/10)});
-                sin2.push({x: i, y: i % 10 == 5 ? null : Math.sin(i/10) *0.25 + 0.5});
-                cos.push({x: i, y: .5 * Math.cos(i/10+ 2) + Math.random() / 10});
-            }
-
-            //Line chart data should be sent as an array of series objects.
-            return [
-                {
-                    values: sin,      //values - represents the array of {x,y} data points
-                    key: 'Sine Wave', //key  - the name of the series.
-                    color: '#ff7f0e'  //color - optional: choose your own line color.
-                },
-                {
-                    values: cos,
-                    key: 'Cosine Wave',
-                    color: '#2ca02c'
-                },
-                {
-                    values: sin2,
-                    key: 'Another sine wave',
-                    color: '#7777ff',
-                    area: true      //area - set to true if you want this line to turn into a filled area chart.
-                }
-            ];
-        };
-
-        var loadData = $.csv.toArrays(loadContent);
-    });
-
-angular.module('partialsApplication').directive('fileReader', function() {
-  return {
-    scope: {
-      fileReader:"="
-    },
-    link: function(scope, element) {
-      $(element).on('change', function(changeEvent) {
-        var files = changeEvent.target.files;
-        if (files.length) {
-          var r = new FileReader();
-          r.onload = function(e) {
-              var contents = e.target.result;
-              scope.$apply(function () {
-                scope.fileReader = contents;
-              });
-          };
-          
-          r.readAsText(files[0]);
-        }
-      });
-    }
-  };
+        
 });
+
 //neccessary for the partials - would like to get rid of if possible
 
 // angular.module('partialsApplication').controller('NKafkaResourceController', ['$scope', 'nKafkaResource',
